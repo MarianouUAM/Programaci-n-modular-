87100e5c8c8fa1b9ae7632698e80c8eb666bda62
MarianouUAM <165739905+MarianouUAM@users.noreply.github.com>
2024-10-20 20:26:55 -0600

    areaTriangulo

---
+++ AreaTriangulo/AreaTriangulo/Program.cs
@@ -0,0 +1,48 @@
+﻿/*using System;
+    public class AreaTriangulo
+    {
+        public static void Main (string[] args)
+        {
+        Console.WriteLine("Ingrese la base del triangulo");
+        double baseT = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la altura del triangulo");
+        double alturaT = Convert.ToDouble(Console.ReadLine());  
+
+        double res = Area(baseT, alturaT);
+        Console.WriteLine($"El area del triangulo es: {res}");
+        Console.ReadKey();
+        }
+
+         public static double Area(double x, double y)
+        {
+            return (x * y) / 2;
+
+        }
+        
+    }*/
+
+using System;
+public class AreaTriangulo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la base del triangulo");
+        double baseT = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la altura del triangulo");
+        double alturaT = Convert.ToDouble(Console.ReadLine());
+
+        double res = 0;
+        Area(ref baseT, ref alturaT, ref res);
+        Console.WriteLine($"El area del triangulo es: {res}");
+        Console.ReadKey();
+    }
+
+    public static void Area(ref double x, ref double y, ref double res)
+    {
+        res = ((x * y) / 2);
+
+    }
+
+}
